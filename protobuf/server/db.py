47edc399b1a1b80e7ada6a2be48c541e67404f0c
Caio Miglioli <caiomiglioli@gmail.com>
2023-05-08 12:27:04 -0300

    init protobuf

---
+++ protobuf/server/db.py
@@ -0,0 +1,36 @@
+from pymongo import MongoClient
+import pprint
+
+#import os
+# from dotenv import load_dotenv
+#from urllib.parse import quote_plus # used to escape string
+
+#load_dotenv()
+DB_USER = 'caiomiglioli'
+DB_PASSWORD = 'miglioli'
+DB_CONNECT_URL = 'sistemas-distribuidos.pcp4inu.mongodb.net/?retryWrites=true&w=majority'
+
+class MongoDBClient:
+    def __init__(self):
+        uri = f"mongodb+srv://{DB_USER}:{DB_PASSWORD}@{DB_CONNECT_URL}"
+        self.client = MongoClient(uri)
+        self.db = self.client['sample_mflix']
+        self.collection = self.db['movies']
+
+    def get_client(self):
+        return self.db
+    
+    def getByActor(self, actor):
+        movies = self.collection.find({"cast": { "$in" : [actor]}})
+        return list(movies)
+    
+    def getByGenre(self, genre):
+        movies = self.collection.find({"genres": { "$in" : [genre]}})
+        return list(movies)
+#end dbclient
+
+
+if __name__ == "__main__":
+    db = MongoDBClient()
+    x = db.getByGenre('Animation')
+    pprint.pprint(x)
---
+++ protobuf/server/server.py
@@ -0,0 +1,55 @@
+import threading
+import socket
+import os
+
+def main():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        server.bind(('localhost', 7777))
+        server.listen()
+    except:
+        return print('\nErro ao iniciar o servidor.')
+
+    # Para cada nova conexão, é gerado uma thread que executa a função
+    # 'orchestra', que por sua vez, aguarda os comandos do cliente.
+    while True:
+        client, ip = server.accept()
+        thread = threading.Thread(target=orchestra, args=[client])
+        thread.start()
+#end main
+
+def orchestra(client):
+    """
+    Função responsável por orquestrar os comandos recebidos do cliente,
+    chamando as funções corretas baseadas no código do comando recebido
+    no pacote do cliente.
+
+    Esta função é executada em uma thread separada para cada usuário conectado.
+    Recebe um socket 'Client' como argumento.
+    """
+
+    print('cliente conectado')
+    sessionUser = None
+    pwd = ''
+    
+    while True:
+        try: 
+            data = client.recv(2048).decode('utf-8')           
+            print(data)
+            # match cmdId:
+            #     #connect
+            #     case 0:
+            #         sessionUser = handleLogin(client, frame)
+
+        except Exception as e:
+            print(e)
+            print("cannot receive data")
+            break
+
+        if not data: 
+            break
+
+    print('cliente perdeu a conexão')
+    client.close()
+#end orchestra
